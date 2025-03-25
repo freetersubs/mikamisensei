3ebc5288e2bc3e6730e24a326bc8053bbdd614e1
FreeterMan <115562343+freetersubs@users.noreply.github.com>
2025-03-25 19:26:48 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function changeBg(bg) {
+  const hero = document.querySelector('.hero');
+  // console.log(bg + 1);
+  //   const contents = document.querySelectorAll('content');
+  // hero.style.background = `url("../img/${bg}")`;
+  hero.style.backgroundSize = 'cover';
+  // hero.style.backgroundPosition = 'bottom';
+
+  //   contents.forEach((content) => {
+  //     content.classList.remove('active');
+  //     if (content.classList.contains(title)) {
+  //       content.classList.add('active');
+  //     }
+  //   });
+}
